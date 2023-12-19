1d727266a070ac7143c44fd5e2b5a2f8a08f65a8
AgustinCamassa <agustincamassa553@outlook.com>
2023-12-19 08:44:42 -0300

    fist commit

--- Desafios en clase/desafiocamassa/src/firebase/database.js
+++
@@ -1,1 +0,0 @@
-export const base_url = "https://react-native-391f9-default-rtdb.firebaseio.com/";
--- Desafios en clase/desafiocamassa/src/theme/colors.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
-  heavyBlue: "#FCE205",
-  mediumBlue: "#FCE205",
-  lightBlue: "#FCE205",
-  white: "#EEEEEE",
-  pink: "#030101",
-};
